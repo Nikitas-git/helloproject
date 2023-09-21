1653268869eb5adb8551cddd0cc08b1a4f5476af
Nikitas-git <142219921+Nikitas-git@users.noreply.github.com>
2023-09-21 12:25:16 +0530

    Update HelloprojectApplication.java

--- src/main/java/com/example/helloproject/HelloprojectApplication.java
+++ src/main/java/com/example/helloproject/HelloprojectApplication.java
@@ -2,9 +2,18 @@ package com.example.helloproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class HelloprojectApplication {
+public class HelloprojectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloprojectApplication.class, args);
